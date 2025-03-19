add25a078626a4ef7c0744b18b632f01ebd60c0f
stepanfedorovv <fedorovsd2005@gmail.com>
2025-03-20 01:12:42 +0500

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,31 +1,21 @@
 function check() {
-
-    var checkBox = document.getElementById("checbox");
-  
+    var checkBox = document.getElementById("checkbox");
     var text1 = document.getElementsByClassName("text1");
-  
     var text2 = document.getElementsByClassName("text2");
-  
-  
-  
+
     for (var i = 0; i < text1.length; i++) {
-  
-      if (checkBox.checked == true) {
-  
-        text1[i].style.display = "block";
-  
-        text2[i].style.display = "none";
-  
-      } else if (checkBox.checked == false) {
-  
-        text1[i].style.display = "none";
-  
-        text2[i].style.display = "block";
-  
-      }
-  
+        if (checkBox.checked) {
+            text1[i].style.display = "none"; // Скрываем 35ml
+            text2[i].style.display = "block"; // Показываем 70ml
+        } else {
+            text1[i].style.display = "block"; // Показываем 35ml
+            text2[i].style.display = "none"; // Скрываем 70ml
+        }
     }
-  
-  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    check();
+});
   
   check();
